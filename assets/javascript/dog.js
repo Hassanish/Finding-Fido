2e1920b3eabef25f400f9d82f9855a276b0559a5
Carolyn Smith <casmith205@gmail.com>
2018-03-21 15:24:12 -0500

    API console logging

--- assets/javascript/dog.js
+++ assets/javascript/dog.js
@@ -1,13 +1,19 @@
+$.getJSON('http://api.petfinder.com/pet.getRandom?format=json&key=359eebb45ac45f0d4449411094f651f8&callback=?')
+  .done(function(petApiData){ console.log(petApiData)});
+  
 
   function animal (){
     var apiKey = "359eebb45ac45f0d4449411094f651f8";
-    var queryURL = "http://api.petfinder.com/pet.find?key=" + apiKey;
+    var queryURL = "http://api.petfinder.com/pet.getRandom?format=json&animal=dog&key=" + apiKey;
 
     $.ajax({
       url: queryURL,
-      method: "GET"
+      jsonp: "callback",
+      dataType: "jsonp"
     }).then(function(response) {
     console.log(response);
     });
 
   };
+
+  animal();
