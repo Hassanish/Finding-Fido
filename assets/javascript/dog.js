9e42a6f7f0de55b6b30f780e51ed38a848e95842
Carolyn Smith <casmith205@gmail.com>
2018-03-21 17:14:59 -0500

    the dog API is pulling correctly from Petfinder

--- assets/javascript/dog.js
+++ assets/javascript/dog.js
@@ -1,19 +1,25 @@
-$.getJSON('http://api.petfinder.com/pet.getRandom?format=json&key=359eebb45ac45f0d4449411094f651f8&callback=?')
-  .done(function(petApiData){ console.log(petApiData)});
-  
+$(document).ready(function(){
 
-  function animal (){
-    var apiKey = "359eebb45ac45f0d4449411094f651f8";
-    var queryURL = "http://api.petfinder.com/pet.getRandom?format=json&animal=dog&key=" + apiKey;
+
+function animal (){
+  var apiKey = "359eebb45ac45f0d4449411094f651f8";
+  var queryURL = "http://api.petfinder.com/pet.getRandom?format=json&output=full&animal=dog&key=" + apiKey;
 
     $.ajax({
-      url: queryURL,
-      jsonp: "callback",
-      dataType: "jsonp"
-    }).then(function(response) {
-    console.log(response);
-    });
-
+      type : 'GET',
+      data : {},
+      url : queryURL+'&callback=?' ,
+      dataType: 'json',
+      success : function(data) { 
+        console.log(data);
+      },
+      error : function (request, error)
+      {
+        alert("Request: "+JSON.stringify(request));
+      }
+    })
   };
 
   animal();
+
+});
