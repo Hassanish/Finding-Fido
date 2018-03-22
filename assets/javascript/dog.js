c8ec078f878d7bd2fb9d504625cf312f0e05a889
Carolyn Smith <casmith205@gmail.com>
2018-03-21 20:44:40 -0500

    added variables to get information from the API in usable formats

--- assets/javascript/dog.js
+++ assets/javascript/dog.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-
+// Creating the animal function to pull information from the Petfinder API
 function animal (){
   var apiKey = "359eebb45ac45f0d4449411094f651f8";
   var queryURL = "http://api.petfinder.com/pet.getRandom?format=json&output=full&animal=dog&key=" + apiKey;
@@ -12,6 +12,35 @@ function animal (){
       dataType: 'json',
       success : function(data) { 
         console.log(data);
+        // Creating a variable to shorten path to get to dog info
+        var petInfo = data.petfinder.pet
+        console.log(petInfo);
+        // Var to determine age of the pet 
+        var petAge = petInfo.age.$t;
+        console.log(petAge);
+        // Var to determine the breed of the pet. If multiple breeds, set the first value 
+        // equal to the breed value
+        if(petInfo.breeds.breed[0] != undefined){
+          var petBreed = petInfo.breeds.breed[0];
+          petBreed = petBreed.$t;
+          console.log(petBreed);
+        } else {
+          var petBreed = petInfo.breeds.breed.$t;
+          console.log(petBreed);
+        };
+        // Finding zip code of shelter
+        var shelterZip = petInfo.contact.zip.$t;
+        console.log(shelterZip);
+        // Finding the email contact of shelter
+        var shelterEmail = petInfo.contact.email.$t; 
+        console.log(shelterEmail);
+        // Loading photos of the dog into an array
+        var dogPhotos = [];
+        for(i=0; i<petInfo.media.photos.photo.length; i++){
+          var currentPhoto = petInfo.media.photos.photo[i];
+          dogPhotos.push(currentPhoto.$t);
+          console.log(dogPhotos);
+        }
       },
       error : function (request, error)
       {
@@ -20,6 +49,10 @@ function animal (){
     })
   };
 
-  animal();
+
+
+
+
+animal();
 
 });
