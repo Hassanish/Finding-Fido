c5884205c1907f7e59e273db98220bdde1ac0401
Carolyn Smith <casmith205@gmail.com>
2018-03-23 09:36:28 -0500

    added in additional variables for background info on dog

--- assets/javascript/dog.js
+++ assets/javascript/dog.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
 
+var petInfo;
+var petName;
+var petAge;
+var petGender;
+var petBreed;
+var petSize;
+var breedSite;
+var petDesc;
+var petNeuter = "Unknown";
+var petShots = "Unknown";
+var petKids = "Unknown";
+var petHouseTrained = "Unknown";
+var shelterEmail;
+var shelterPhone;
+var shelterZip;
+var dogPhotos =[];
+
 // Creating the animal function to pull information from the Petfinder API
 function animal (){
   var apiKey = "359eebb45ac45f0d4449411094f651f8";
@@ -12,16 +29,17 @@ function animal (){
       dataType: 'json',
       success : function(data) { 
         // Creating a variable to shorten path to get to dog info
-        var petInfo = data.petfinder.pet
+        petInfo = data.petfinder.pet
         console.log(petInfo);
-        // Var to determine name of the pet
-        var petName = petInfo.name.$t;
-        console.log(petName);
-        // Var to determine age of the pet 
-        var petAge = petInfo.age.$t;
-        console.log(petAge);
-        // Var to determine gender of the pet
-        var petGender = petInfo.sex.$t;
+        // Name of the pet
+        petName = petInfo.name.$t;
+
+        // Age of the pet 
+        petAge = petInfo.age.$t;
+
+        // Gender of the pet
+        petGender = petInfo.sex.$t;
+        // Converting letter into full word
         if(petGender == "M"){
           petGender = "Male"
         } else {
@@ -30,11 +48,11 @@ function animal (){
         console.log(petGender);
         // Var to determine the breed of the pet. If multiple breeds, set the first value equal to the breed value
         if(petInfo.breeds.breed[0] != undefined){
-          var petBreed = petInfo.breeds.breed[0];
+          petBreed = petInfo.breeds.breed[0];
           petBreed = petBreed.$t;
           console.log(petBreed);
         } else {
-          var petBreed = petInfo.breeds.breed.$t;
+          petBreed = petInfo.breeds.breed.$t;
           console.log(petBreed);
         };
         // Changing pit bull terrier to AKC name
@@ -43,7 +61,7 @@ function animal (){
         };
         //Splitting the bread in separate strings
         var breedSplit = petBreed.split(" ");
-        var breedSite = "http://www.akc.org/dog-breeds/"
+        breedSite = "http://www.akc.org/dog-breeds/"
         //Add each string to the URL to link to the AKC site with the correct page
         for(i=0; i<breedSplit.length; i++){
           var breed = breedSplit[i];
@@ -54,21 +72,59 @@ function animal (){
           };
         };
         console.log(breedSite);
-        
+
+        // Breed size 
+        petSize = petInfo.size.$t;
+
+        // Converting size into full word
+        if(petSize == "L"){
+          petSize = "Large";
+        } else if (petSize == "M"){
+          petSize = "Medium";
+        } else if (petSize == "S"){
+          petSize = "Small";
+        } else {
+          petSize = "Extra Large"
+        };
+
         // Descr from shelter
-        var petDesc = petInfo.description.$t;
+        petDesc = petInfo.description.$t;
      
+        // Background info on dog (neutered, vaccinated, good with kids, housetrained)
+        var petOptions = petInfo.options.option;
+        console.log(petOptions);
+        if(petOptions != undefined){
+          for(i=0; i<petOptions.length; i++){
+            if(petOptions[i].$t == "altered"){
+              petNeuter = "Neutered/Spayed";
+            };
+            if(petOptions[i].$t == "hasShots"){
+              petShots = "Vaccinated";
+            };
+            if(petOptions[i].$t == "noKids"){
+              petKids = "Not good with children";
+            };
+            if(petOptions[i].$t == "housetrained"){
+              petHouseTrained = "House trained";
+            };
+          };
+        };
+        console.log(petNeuter);
+        console.log(petShots);
+        console.log(petKids);
+        console.log(petHouseTrained);
+        
         // Zip code of shelter
-        var shelterZip = petInfo.contact.zip.$t;
+        shelterZip = petInfo.contact.zip.$t;
 
         // Email contact of shelter
-        var shelterEmail = petInfo.contact.email.$t; 
+        shelterEmail = petInfo.contact.email.$t; 
 
         // Phone number of the shelter
-        var shelterPhone = petInfo.contact.phone.$t;
+        shelterPhone = petInfo.contact.phone.$t;
 
         // Loading photos of the dog into an array
-        var dogPhotos = [];
+        dogPhotos = [];
         for(i=0; i<petInfo.media.photos.photo.length; i++){
           var currentPhoto = petInfo.media.photos.photo[i];
           dogPhotos.push(currentPhoto.$t);
@@ -83,10 +139,12 @@ function animal (){
     })
   };
 
+  animal();
+// On button click, run the following fuctions
+$(".button").on("click", function(){
+  animal();
+})
 
 
-
-
-animal();
 
 });
